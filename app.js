f1f2187d06ed1f3493e7bfb23bf2c04e09cac82a
Guilherme Santiago <guilhermebscontact@gmail.com>
2022-06-07 22:24:22 -0300

    Finishing the project

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
-let pastMusicButton = document.querySelectorAll(".music-button")[0];
+let previousMusicButton = document.querySelectorAll(".music-button")[0];
 let pauseMusicButton = document.querySelectorAll(".music-button")[1];
 let playMusicButton = document.querySelectorAll(".music-button")[2];
 let nextMusicButton = document.querySelectorAll(".music-button")[3];
 let music = document.querySelector("audio")
+let musicIndex = 0 ;
+let musicsProps = [
+    {
+        title: "Ainda gosto dela - ",
+        artist: "Skank",
+        img: "./img/aindagostodela.png",
+        src: "./music/Ainda Gosto Dela.mp3"
+    },
+    {
+        title: "Despedida - ",
+        artist: "Selvagens à procura da lei",
+        img: "./img/despedida.jpg",
+        src: "./music/Despedida.mp3"
+    },
+    {
+        title: "Exagerado - ",
+        artist: "Cazuza",
+        img: "./img/exagerado.webp",
+        src: "./music/Exagerado.mp3"
+    }
+]
+let musicTitle = document.querySelector("#music-name")
+let musicArtist = document.querySelector("#artist")
+let musicImage = document.querySelector(".music-photo img")
 
-pastMusicButton.addEventListener("click", () => {
+window.onload = renderMusic(0)
 
+//Events
+previousMusicButton.addEventListener("click", () => {
+    if(musicIndex <= 0){
+        alert("Acabaram as músicas :(")
+    }
+    musicIndex--
+    renderMusic(musicIndex)
 })
 
 pauseMusicButton.addEventListener("click", () => {
@@ -21,7 +52,11 @@ playMusicButton.addEventListener("click", () => {
 })
 
 nextMusicButton.addEventListener("click", () => {
-    
+    if(musicIndex >=(musicsProps.length - 1)){
+        alert("Acabaram as músicas :(")
+    }
+    musicIndex++
+    renderMusic(musicIndex)
 })
 
 music.addEventListener("timeupdate", () => {
@@ -32,9 +67,24 @@ music.addEventListener("timeupdate", () => {
     musicTime.textContent = secondsForMinutes(Math.floor(music.currentTime))
 })
 
+
+//Functions
 function secondsForMinutes(seconds){
     let minuteSpace = Math.floor(seconds/60);
     let secondSpace = seconds%60
     if(secondSpace < 10) { secondSpace = `0${secondSpace}` }
-    if(minuteSpace<10) { return "0"+minuteSpace+":"+secondSpace }else{ return minuteSpace+":"+secondSpace}
+    if(minuteSpace<10) { 
+        return "0"+minuteSpace+":"+secondSpace
+     }else{
+        return minuteSpace+":"+secondSpace
+     }
+}
+
+function renderMusic(index){
+    music.setAttribute("src", musicsProps[index].src)
+    music.addEventListener("loadeddata", () =>{
+        musicTitle.textContent = musicsProps[index].title
+        musicArtist.textContent = musicsProps[index].artist
+        musicImage.src = musicsProps[index].img
+    })
 }
