4ea0b8b6d5159428f68ace804c4e52ee8e26da4e
Guilherme Santiago <guilhermebscontact@gmail.com>
2022-06-09 18:16:26 -0300

    added volume in player, fixed the bug of stop playing music after click in the skip music button

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ let previousMusicButton = document.querySelectorAll(".music-button")[0];
 let pauseMusicButton = document.querySelectorAll(".music-button")[1];
 let playMusicButton = document.querySelectorAll(".music-button")[2];
 let nextMusicButton = document.querySelectorAll(".music-button")[3];
-let music = document.querySelector("audio")
+let volumeButton = document.querySelectorAll(".music-button")[4];
+let dropUp = document.querySelector(".drop-up")
+let volumeBar = document.querySelector("#volume-bar");
+let music = document.querySelector("audio");
 let musicIndex = 0 ;
+let buttonForVolume = 0;
 let musicsProps = [
     {
         title: "Ainda gosto dela - ",
@@ -28,7 +32,7 @@ let musicTitle = document.querySelector("#music-name")
 let musicArtist = document.querySelector("#artist")
 let musicImage = document.querySelector(".music-photo img")
 
-window.onload = renderMusic(0)
+window.onload = renderMusic(0), music.volume = volumeBar.value/100
 
 //Events
 previousMusicButton.addEventListener("click", () => {
@@ -37,6 +41,7 @@ previousMusicButton.addEventListener("click", () => {
         musicIndex = 2
     }
     renderMusic(musicIndex)
+    playMusic()
 })
 
 pauseMusicButton.addEventListener("click", () => {
@@ -46,9 +51,7 @@ pauseMusicButton.addEventListener("click", () => {
 })
 
 playMusicButton.addEventListener("click", () => {
-    music.play()
-    document.querySelector(".play-music").style.display = "none"
-    document.querySelector(".pause-music").style.display = "block"
+    playMusic()
 })
 
 nextMusicButton.addEventListener("click", () => {
@@ -57,6 +60,7 @@ nextMusicButton.addEventListener("click", () => {
         musicIndex = 0
     }
     renderMusic(musicIndex)
+    playMusic()
 })
 
 music.addEventListener("timeupdate", () => {
@@ -65,6 +69,34 @@ music.addEventListener("timeupdate", () => {
     bar.setAttribute("style", `width: ${currentMomentOMusic}%`)
     let musicTime = document.querySelector("#time")
     musicTime.textContent = secondsForMinutes(Math.floor(music.currentTime))
+})
+
+volumeBar.addEventListener("change", () => {
+    music.volume = volumeBar.value/100
+    let volumeIcon = document.querySelector(".volume-icon")
+    if(volumeBar.value/100 == 0){
+        volumeIcon.setAttribute("data-feather", "volume-x")
+        feather.replace()
+    }else if(volumeBar.value > 0 && volumeBar.value<= 25){
+        volumeIcon.setAttribute("data-feather", "volume")
+        feather.replace()
+    }else if(volumeBar.value > 25 && volumeBar.value<= 50){
+        volumeIcon.setAttribute("data-feather", "volume-1")
+        feather.replace()
+    }else{
+        volumeIcon.setAttribute("data-feather", "volume-2")
+        feather.replace()
+    }
+})
+
+volumeButton.addEventListener("click", () => {
+    if(buttonForVolume == 0){
+        dropUp.style.display = "block" 
+        buttonForVolume++
+    }else{
+        dropUp.style.display = "none"
+        buttonForVolume--
+    }
 })
 
 
@@ -78,6 +110,12 @@ function secondsForMinutes(seconds){
      }else{
         return minuteSpace+":"+secondSpace
      }
+}
+
+function playMusic(){
+    music.play()
+    document.querySelector(".play-music").style.display = "none"
+    document.querySelector(".pause-music").style.display = "block"
 }
 
 function renderMusic(index){
